7ff13b7399b7e196fe8e98b978679dcd6994ef4e
Sam Delacruz <sam@samdlc.com>
2015-02-09 16:11:32 +0000

    Improvements to photo posting UI flow. Separated logic from view in navbar and index. Created dummy form for posting overlay text on image.

--- inc/scripts.php
+++ inc/scripts.php
@@ -7,25 +7,4 @@
 <script src="vendor/kartik-v/bootstrap-fileinput/js/fileinput.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <script src="js/post_update.js"></script>
-<script>
-    $("#fb_image").fileinput(({
-        'uploadAsync' : false,
-        'allowedFileExtensions' : ['jpg', 'jpeg', 'png','gif'],
-        'maxFilesNum' : 1,
-        'uploadUrl' : 'http://localhost:8888/photo.php',
-        'dropZoneEnabled' : false,
-        'layoutTemplates' : {
-           actionUpload: ''
-        },
-        'uploadExtraData' : function () {
-            return {'fb_status' : $("#fb_status").val()};
-        }
-    }));
-
-    $('#fb_image').on('fileuploaded', function(event, data, previewId, index) {
-        var form = data.form, files = data.files, extra = data.extra,
-            response = data.response, reader = data.reader;
-        console.log(response);
-    });
-    
-</script>
+<script src="js/post_photo.js"></script>
---
+++ js/post_overlay.js
@@ -0,0 +1,3 @@
+/**
+ * Created by sdelacruz on 09/02/15.
+ */
