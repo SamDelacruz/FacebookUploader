1b6bce949bfd45dfefc5af1c69d3681ebcdbe4c9
Sam Delacruz <sam@samdlc.com>
2015-02-12 21:22:03 +0000

    Reorganise assets

--- inc/scripts.php
+++ inc/scripts.php
@@ -4,7 +4,7 @@
  */
 ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script src="vendor/kartik-v/bootstrap-fileinput/js/fileinput.min.js"></script>
+<script src="js/fileinput.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <script src="js/post_status.js"></script>
 <script src="js/post_photo.js"></script>
